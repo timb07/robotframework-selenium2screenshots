7a6d550632fc299eb5411306e5a780504d8cab14
Tim Bell <timothybell@gmail.com>
2018-11-05 15:48:47 +1100

    Hack to support deviceScaleFactor in Chrome

--- src/Selenium2Screenshots/__init__.py
+++ src/Selenium2Screenshots/__init__.py
@@ -2,6 +2,7 @@
 import os.path
 
 from robot.libraries.BuiltIn import BuiltIn
+from SeleniumLibrary.base import keyword, LibraryComponent
 
 
 class Selenium2Screenshots(object):
@@ -16,6 +17,12 @@ class Selenium2Screenshots(object):
         """Import Selenium2Screenshots user keywords.
         """
         BuiltIn().import_resource('Selenium2Screenshots/keywords.robot')
+        sl = BuiltIn().get_library_instance('Selenium2Library')
+        sl.add_library_components([SeleniumChromeKeywords(sl)])
+        BuiltIn().reload_library('Selenium2Library')
+
+
+scale_factor = 1.0
 
 
 class Image(object):
@@ -26,9 +33,34 @@ class Image(object):
         from PIL import Image
 
         img = Image.open(os.path.join(output_dir, filename))
+        left, top, width, height = (
+           left * scale_factor, top * scale_factor,
+           width * scale_factor, height * scale_factor
+        )
         box = (int(left), int(top), int(left + width), int(top + height))
 
         area = img.crop(box)
 
         with open(os.path.join(output_dir, filename), 'wb') as output:
             area.save(output, 'png')
+
+
+class SeleniumChromeKeywords(LibraryComponent):
+
+    def __init__(self, ctx):
+        LibraryComponent.__init__(self, ctx)
+
+    @keyword
+    def set_devicescalefactor(self, factor):
+        factor = float(factor)
+        self.ctx.driver.execute_cdp_cmd(
+            "Page.setDeviceMetricsOverride",
+            {
+                "deviceScaleFactor": factor,
+                "width": 0,
+                "height": 0,
+                "mobile": False,
+            }
+        )
+        global scale_factor
+        scale_factor = factor
